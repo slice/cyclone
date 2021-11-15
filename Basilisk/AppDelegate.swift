b2630a64fb8a7b2b1a7f96ddcdf1dfc5343e9136
slice <tinyslices@gmail.com>
2021-11-14 19:28:03 -0800

    feat: gracefully close clients when terminating
    We can take advantage of applicationShouldTerminate to asynchronously
    close clients before letting the app terminate.

--- Basilisk/AppDelegate.swift
+++ Basilisk/AppDelegate.swift
@@ -4,25 +4,31 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
   var gatewayLogStore = GatewayLogStore()
 
+  var activeViewControllers: [ViewController] {
+    NSApp.windows.compactMap { $0.contentViewController as? ViewController }
+  }
+
   func applicationDidFinishLaunching(_: Notification) {
     // Insert code here to initialize your application
   }
 
-  @MainActor func applicationWillTerminate(_: Notification) {
-    for window in NSApp.windows {
-      if let viewController = window.contentViewController as? ViewController {
-        NSLog(
-          "attempting to disconnect client contained within \(viewController)"
-        )
-
-        // TODO(skip): This should be made synchronous somehow, because we can't
-        // otherwise guarantee the execution of this task before the application
-        // terminates. (And indeed, this doesn't actually seem to execute in
-        // time. Oh well.)
-        Task {
-          try! await viewController.tearDownClient()
+  @MainActor func applicationShouldTerminate(_ sender: NSApplication)
+    -> NSApplication.TerminateReply
+  {
+    if activeViewControllers.isEmpty {
+      return .terminateNow
+    } else {
+      Task {
+        for activeViewController in activeViewControllers {
+          NSLog("trying to disconnect client in \(activeViewController)")
+          try! await activeViewController.client?.disconnect()
         }
+
+        sender.reply(toApplicationShouldTerminate: true)
       }
+
+      NSLog("terminating later, there are active view controllers")
+      return .terminateLater
     }
   }
 

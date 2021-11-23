7d89ebfb29eed30a0c0fbce6a020be099f9310a1
slice <tinyslices@gmail.com>
2021-11-23 14:37:58 -0800

    refactor: simplify RoundingView (again)
    wantsLayer = true seems to be unnecessary most of the time.

--- Basilisk/Chat/RoundingView.swift
+++ Basilisk/Chat/RoundingView.swift
@@ -3,16 +3,6 @@ import Cocoa
 class RoundingView: NSView {
   @Invalidating(.display) var radius: CGFloat = 50.0
 
-  override init(frame frameRect: NSRect) {
-    super.init(frame: frameRect)
-    wantsLayer = true
-  }
-
-  @available(*, unavailable)
-  required init?(coder _: NSCoder) {
-    fatalError("no")
-  }
-
   override var wantsUpdateLayer: Bool { true }
 
   override func updateLayer() {

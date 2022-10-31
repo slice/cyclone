acef70db53ae14d1dfef7a1d7730896aa59e363e
slice <tinyslices@gmail.com>
2022-10-31 14:17:08 -0700

    support optional fractional seconds in dates
    Discord inconsistently provides fractional seconds in their timestamps,
    so handle them accordingly.

--- Serpent/SerpentJSONDecoder.swift
+++ Serpent/SerpentJSONDecoder.swift
@@ -5,6 +5,12 @@ import Foundation
 class SerpentJSONDecoder: JSONDecoder {
   override init() {
     super.init()
-    dateDecodingStrategy = .iso8601
+    dateDecodingStrategy = .custom { decoder in
+      let iso8601Timestamp = try decoder.decodeSingleValue(as: String.self)
+
+      // Discord timestamps have fractional seconds, but only some of the time.
+      let formatStyle = Date.ISO8601FormatStyle(includingFractionalSeconds: iso8601Timestamp.contains("."))
+      return try formatStyle.parse(iso8601Timestamp)
+    }
   }
 }

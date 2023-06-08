3625dc65ab0cb4cf2d45eeef6e195aba249e12ed
slice <tinyslices@gmail.com>
2023-06-07 17:22:41 -0700

    throw an error if decompressed data is >16mib

--- Serpent/Gateway/Decompression.swift
+++ Serpent/Gateway/Decompression.swift
@@ -2,14 +2,16 @@ import Compression
 import Foundation
 
 class Decompression {
-  static let bufferSize: Int = 1024 * 1024 * 8
+  static let bufferSize: Int = 1024 * 1024 * 16 // 16 mebibytes
   var streamPointer: UnsafeMutablePointer<compression_stream>
   var destinationBuffer: UnsafeMutablePointer<UInt8>
 
+  /// An error thrown during decompression.
   enum Error: Swift.Error {
     case initialization
     case incomplete
     case processing
+    case tooLarge
   }
 
   init() throws {
@@ -23,7 +25,7 @@ class Decompression {
     streamPointer.pointee.dst_size = Self.bufferSize
   }
 
-  func initializeStream() throws {
+  private func initializeStream() throws {
     self.streamPointer = .allocate(capacity: 1)
     let status = compression_stream_init(self.streamPointer, COMPRESSION_STREAM_DECODE, COMPRESSION_ZLIB)
     guard status != COMPRESSION_STATUS_ERROR else {
@@ -31,6 +33,11 @@ class Decompression {
     }
   }
 
+  /// Decompress a raw buffer of `DEFLATE` compressed bytes, returning the
+  /// original data.
+  ///
+  /// The buffer shouldn't contain a zlib header---for example, the bytes
+  /// `78 da`.
   func decompress(completeBuffer data: Data) throws -> Data {
     try data.withUnsafeBytes { untypedPointer in
       let sourcePointer = untypedPointer.bindMemory(to: UInt8.self).baseAddress!
@@ -40,6 +47,10 @@ class Decompression {
       self.resetDestination()
 
       let status = compression_stream_process(self.streamPointer, 0)
+
+      guard streamPointer.pointee.dst_size != 0 else {
+        throw Error.tooLarge
+      }
 
       guard status == COMPRESSION_STATUS_OK else {
         throw Error.processing
